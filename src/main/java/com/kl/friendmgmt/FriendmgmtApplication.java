a5d61eda00d6c063098ee6d24824ce98b6ad4f7a
Goh Kian Leong <kianleong@live.com.sg>
2017-10-03 16:07:20 +0800

    Initial Commit

---
+++ src/main/java/com/kl/friendmgmt/FriendmgmtApplication.java
@@ -0,0 +1,12 @@
+package com.kl.friendmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendmgmtApplication.class, args);
+	}
+}
